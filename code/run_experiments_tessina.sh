2a8d80f4c355c6bee03cef353b93a2f28d33096d
Donato D'Ambrosio <donato.dambrosio@unical.it>
2026-02-16 16:57:06 +0100

    Updated output file name format

--- code/run_experiments_tessina.sh
+++ code/run_experiments_tessina.sh
@@ -10,26 +10,26 @@ IN_SRC="../data/tessina_source.txt"
 
 
 EXE="./sciddicaT_naive"
-OUT="./tessina_output_naive"
+OUT="./output_tessina_naive"
 MESSAGE="TESSINA_EXPERIMENTS_naive"
 run_experiments
 
 EXE="./sciddicaT_tiled_basic_halo"
-OUT="./tessina_output_tiled_basic_halo"
+OUT="./output_tessina_tiled_basic_halo"
 MESSAGE="TESSINA_EXPERIMENTS_tiled_basic_halo"
 run_experiments
 
 EXE="./sciddicaT_tiled_basic_no_halo"
-OUT="./tessina_output_tiled_basic_no_halo"
+OUT="./output_tessina_tiled_basic_no_halo"
 MESSAGE="TESSINA_EXPERIMENTS_tiled_no_halo"
 run_experiments
 
 EXE="./sciddicaT_tiled_CfAMe"
-OUT="./tessina_output_tiled_CfAMe"
+OUT="./output_tessina_tiled_CfAMe"
 MESSAGE="TESSINA_EXPERIMENTS_tiled_CfAMe"
 run_experiments
 
 EXEC="./sciddicaT_tiled_CfAMo"
-OUT="./tessina_output_tiled_CfAMo"
+OUT="./output_tessina_tiled_CfAMo"
 MESSAGE="TESSINA_EXPERIMENTS_tiled_CfAMo"
 run_experiments
